e787d5d0d9531fb8169a92795925e3e4b6496ef5
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-06-19 12:52:41 +0800

    add router env and handlebars

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const routes = require('./routes')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+
+app.set('view engine', 'hbs')
+app.use(routes)
+
+app.listen(port, () => {
+  console.info(`My-forum app is listening on port ${port}`)
+})
+
+module.express = app
