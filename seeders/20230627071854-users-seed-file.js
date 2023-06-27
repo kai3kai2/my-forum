c8b4e8b4b5bf21b7d400d8cedd2f7088fa1ee319
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-06-27 15:28:39 +0800

    add user seed file

---
+++ seeders/20230627071854-users-seed-file.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [{
+      email: 'user1@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      name: 'user1',
+      cover: 'https://official.yousing.com.tw/wp-content/uploads/2019/07/%E9%A0%90%E8%A8%AD%E9%A0%AD%E5%83%8F-02.jpg',
+      introduction: '打些內容來讓大家認識你吧!',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      name: 'user2',
+      cover: 'https://official.yousing.com.tw/wp-content/uploads/2019/07/%E9%A0%90%E8%A8%AD%E9%A0%AD%E5%83%8F-02.jpg',
+      introduction: '打些內容來讓大家認識你吧!',
+      created_at: new Date(),
+      updated_at: new Date()
+    }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', {})
+  }
+}
